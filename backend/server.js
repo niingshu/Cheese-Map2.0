6a879dba537fbf082488a4edac40964adf6ea339
nshu15 <shuning010506@gmail.com>
2026-02-15 22:43:33 -0800

    established connection with MongoDB and Node.js using Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
-//basic server code 
+//basic server code -> everything in here runs nodejs code
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const PORT = 5000;
+
+//connect with the mongodb 
+mongoose.connect("mongodb://127.0.0.1:27017/cheeses")
+    .then(() => console.log("MongoDB connected successfully"))
+    .catch(err => console.log("MongoDB connected failed", err));
 
 //define a basic GET route
 app.get('/api/cheeses', (req, res) => {
